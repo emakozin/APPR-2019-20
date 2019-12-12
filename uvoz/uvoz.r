708db2976f68e1fcdb24e1f5234ed20f78a87ba1
Ema Kozin <ema.kozin@student.fmf.uni-lj.si>
2019-12-12 09:56:44 +0100

    uvoz podatkov 1.del

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -3,32 +3,56 @@
 
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
+
+  data <- read_csv2("podatki/solskoleto_regija_program.csv",locale=locale(encoding="UTF-8"), na = "-")
+  colnames(data) <- c("Leto","Regija","Predsolska glasbena vzgoja","Glasbena pripravnica","Plesna pripravnica","Glasba","Ples")
+  data$Regija[data$Regija == "Gori?ka"] <- "Goriska"
+  data$Regija[data$Regija == "Koro?ka"] <- "Koroska"
+  data$Regija[data$Regija == "Obalno-kra?ka"] <- "Obalno-kraska" 
+  
+  data2 <- read_csv2("podatki/program_zavod.csv", locale=locale(encoding="UTF-8"), na ="-")
+  colnames(data2) <- c("Leto","Regija","Zavod", "Predsolska glasbena vzgoja","Glasbena pripravnica","Plesna pripravnica","Glasba","Ples")
+  data2$Regija[data2$Regija == "Gori?ka"] <- "Goriska"
+  data2$Regija[data2$Regija == "Koro?ka"] <- "Koroska"
+  data2$Regija[data2$Regija == "Obalno-kra?ka"] <- "Obalno-kraska"
+  data2$Zavod[data2$Zavod == "Javna glasbena ?ola"] <- "Javna glasbena sola"
+  data2$Zavod[data2$Zavod == "Zasebna glasbena ?ola"] <- "Zasebna glasbena sola"
+  
+  
+
+
+
+
+
+
+
+
 # Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
+#uvozi.obcine <- function() {
+#  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+#  stran <- html_session(link) %>% read_html()
+#  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+#   .[[1]] %>% html_table(dec=",")
+# for (i in 1:ncol(tabela)) {
+#    if (is.character(tabela[[i]])) {
+#      Encoding(tabela[[i]]) <- "UTF-8"
+#    }
+# }
+#  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+#                        "ustanovitev", "pokrajina", "regija", "odcepitev")
+#  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+#  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+#  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+#  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+#    if (is.character(tabela[[col]])) {
+#      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
+#    }
+# }
+#  for (col in c("obcina", "pokrajina", "regija")) {
+#    tabela[[col]] <- factor(tabela[[col]])
+#  }
+#  return(tabela)
+#}
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
 uvozi.druzine <- function(obcine) {
